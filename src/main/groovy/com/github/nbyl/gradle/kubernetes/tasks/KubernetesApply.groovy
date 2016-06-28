859552719c22ad2bd4f726402dfeb72d2c3276a7
Nicolas Byl <nico@nicolas-byl.eu>
2016-06-28 16:43:30 +0200

    add apply task

---
+++ src/main/groovy/com/github/nbyl/gradle/kubernetes/tasks/KubernetesApply.groovy
@@ -0,0 +1,34 @@
+package com.github.nbyl.gradle.kubernetes.tasks
+
+import com.github.nbyl.gradle.kubernetes.KubernetesController
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class KubernetesApply extends DefaultTask {
+
+    def KubernetesController controller
+
+    def manifestDirs = ['src/main/kubernetes']
+    def manifestInclude = '**/*.yml'
+
+    KubernetesApply() {
+    }
+
+    def deployFile(def manifest) {
+        project.exec {
+            commandLine 'kubectl', 'apply', '-f', "${manifest}"
+        }
+    }
+
+
+    @TaskAction
+    def deploy() {
+        project.files(manifestDirs).each {
+            manifestDir ->
+                project.fileTree(dir: manifestDir, include: manifestInclude).each {
+                    controller.applyManifest(it)
+                }
+        }
+    }
+
+}
